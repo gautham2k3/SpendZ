cb5042e4e4b761e2c806c9a50d382c475de885f3
Gautham <bgautham27@gmail.com>
2023-05-15 23:57:04 +0530

    Added Sliver App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ void main() {
 int x=0;
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -48,18 +48,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      x++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -70,40 +58,37 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            floating: true,
+            forceElevated: true,
+            leading: IconButton(
+                icon: const Icon(Icons.settings),
+                iconSize:30 ,
+                onPressed: () {}
             ),
-            Text(
-              '$x',
-              style: Theme.of(context).textTheme.headlineMedium,
+            flexibleSpace: const FlexibleSpaceBar(
+              centerTitle: true,
+              title: Text("SpendZ"),
             ),
-          ],
-        ),
+            actions: [
+              IconButton(
+                icon: const Icon(Icons.minimize_rounded),
+                iconSize: 30,
+                onPressed:() {
+                  setState(()
+                  {
+                    if(x>0)
+                    {
+                      x--;
+                    }
+                  });
+                },
+              )
+            ],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
