425d03dad29afa997abc2ea35f97c39ee06ffdfc
Gautham <bgautham27@gmail.com>
2023-06-02 23:03:20 +0530

    Added Tabs Management Class

---
+++ lib/Screens/tabs_manager.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class tabs_manager extends StatefulWidget {
+  const tabs_manager({super.key});
+  @override
+  State<tabs_manager> createState() => _tabs_manager();
+}
+
+class _tabs_manager extends State<tabs_manager> {
+  @override
+  Widget build (BuildContext context)
+  {
+    return Container(color:  Color(Colors.blueAccent as int));
+  }
+}
