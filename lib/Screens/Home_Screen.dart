bd69121ca0b6082df6daaf9d67c1f9c814bd5c8f
Gautham <bgautham27@gmail.com>
2023-09-12 00:06:53 +0530

    Added Base for Balances and Transaction History

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 import 'Settings.dart';
+
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() {
     return _HomeScreenState();
-
   }
 }
 
-
 class _HomeScreenState extends State<HomeScreen> {
+  int balance = 1000; // Example balance value, replace with your actual balance
+  List<String> transactionHistory = [
+    'Transaction 1',
+    'Transaction 2',
+    'Transaction 3',
+    'More to come ',
+    'More and More',
+    'More Transaction',
+    'Transaction 1',
+    'Transaction 2',
+    'Transaction 3',
+    'More to come ',
+    'More and More',
+    'More Transaction',
+    'Transaction 1',
+    'Transaction 2',
+    'Transaction 3',
+    'More to come ',
+    'More and More',
+    'More Transaction',
+    // Add your transaction history items here
+  ];
 
-  int x=0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,45 +41,58 @@ class _HomeScreenState extends State<HomeScreen> {
             floating: true,
             forceElevated: true,
             leading: IconButton(
-                icon: Icon(Icons.settings),
-                iconSize:30 ,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Settings()),
-                  );
-                }
+              icon: Icon(Icons.settings),
+              iconSize: 30,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Settings()),
+                );
+              },
             ),
             flexibleSpace: FlexibleSpaceBar(
               centerTitle: true,
-              title:const  Text("SpendZ"),
+              title: const Text("SpendZ"),
             ),
             actions: [
               IconButton(
                 icon: const Icon(Icons.account_circle_rounded),
                 iconSize: 30,
-                onPressed:() {
-                  setState((){});
+                onPressed: () {
+                  setState(() {});
                 },
               )
             ],
           ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-                  (BuildContext context,int index) {
-                return Container(
-                  margin: const EdgeInsets.all(10.0),
-                  height: 100,
-                  color: Colors.blue[100],
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  alignment: Alignment.center,
                   child: Text(
-                    "Value of x : $x",style: const TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
+                    'Balance Left: \$${balance.toStringAsFixed(2)}',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  child: const Text(
+                    'Transaction History',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
-                );
-              },
-              childCount: 50,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const ClampingScrollPhysics(),
+                  itemCount: transactionHistory.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      title: Text(transactionHistory[index]),
+                    );
+                  },
+                ),
+              ],
             ),
           ),
         ],
