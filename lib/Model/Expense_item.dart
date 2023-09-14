abd853e890edffddcc003bf59d687d7b519a1b79
Gautham <bgautham27@gmail.com>
2023-09-15 00:00:44 +0530

    Added Data Structure

---
+++ lib/Model/Expense_item.dart
@@ -0,0 +1,11 @@
+class ExpenseItem {
+  final String name;
+  final DateTime dateTime;
+  final String amount;
+
+  ExpenseItem({
+    required this.name,
+    required this.dateTime,
+    required this.amount
+  });
+}
