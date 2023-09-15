a0b254f95ed0548a1095b7b8250f12a8ef98b8fc
Gautham <bgautham27@gmail.com>
2023-09-15 23:32:42 +0530

    Transaction History Working

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import '../Data/Expense_data.dart';
 import 'Settings.dart';
+import 'package:provider/provider.dart';
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -10,93 +13,102 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int balance = 1000; // Example balance value, replace with your actual balance
-  List<String> transactionHistory = [
-    'Transaction 1',
-    'Transaction 2',
-    'Transaction 3',
-    'More to come ',
-    'More and More',
-    'More Transaction',
-    'Transaction 1',
-    'Transaction 2',
-    'Transaction 3',
-    'More to come ',
-    'More and More',
-    'More Transaction',
-    'Transaction 1',
-    'Transaction 2',
-    'Transaction 3',
-    'More to come ',
-    'More and More',
-    'More Transaction',
-    // Add your transaction history items here
-  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            floating: true,
-            forceElevated: true,
-            leading: IconButton(
-              icon: Icon(Icons.settings),
-              iconSize: 30,
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Settings()),
-                );
-              },
-            ),
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              title: const Text("SpendZ"),
-            ),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.account_circle_rounded),
-                iconSize: 30,
+    return Consumer<ExpenseData>(
+      builder: (context,value,child) => Scaffold(
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              floating: true,
+              forceElevated: true,
+              leading: IconButton(
+                icon: const Icon(Icons.settings),
+                iconSize:30 ,
                 onPressed: () {
-                  setState(() {});
+                  // Open the Add Transaction dialog
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return Settings();
+                    },
+                  );
                 },
-              )
-            ],
-          ),
-          SliverToBoxAdapter(
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(16.0),
-                  alignment: Alignment.center,
-                  child: Text(
-                    'Balance Left: \$${balance.toStringAsFixed(2)}',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  child: const Text(
-                    'Transaction History',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                ListView.builder(
-                  shrinkWrap: true,
-                  physics: const ClampingScrollPhysics(),
-                  itemCount: transactionHistory.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                      title: Text(transactionHistory[index]),
-                    );
+              ),
+              flexibleSpace:const FlexibleSpaceBar(
+                centerTitle: true,
+                title: Text("SpendZ"),
+              ),
+              actions: [
+                IconButton(
+                  icon: const Icon(Icons.account_circle_rounded),
+                  iconSize: 30,
+                  onPressed: () {
+                    setState(() {});
                   },
-                ),
+                )
               ],
             ),
-          ),
-        ],
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                  return Container();
+                },
+                childCount: 10,
+              ),
+            ),
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Container(
+                    padding:const EdgeInsets.all(16.0),
+                    alignment: Alignment.center,
+                    child: Text(
+                      'Balance Left: \$${balance.toStringAsFixed(2)}',
+                      style:const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Container(
+                    padding:const EdgeInsets.fromLTRB(0,225,0,0),
+                    child: const Text(
+                      'Transaction History',
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: value.getExpenseList().length,
+                    physics: const ClampingScrollPhysics(),
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text(value.getExpenseList()[index].name),
+                        subtitle: Text(
+                            '${value.getExpenseList()[index].dateTime.hour}:'
+                                '${value.getExpenseList()[index].dateTime.minute} / '
+                                '${value.getExpenseList()[index].dateTime.day}.'
+                                '${value.getExpenseList()[index].dateTime.month}.'
+                                '${value.getExpenseList()[index].dateTime.year.toString().substring(2)}'
+                        ),
+                        trailing: Text('₹${value.getExpenseList()[index].amount}',
+                          style: const TextStyle(
+                              fontSize: 20,fontWeight: FontWeight.bold
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
+
+
