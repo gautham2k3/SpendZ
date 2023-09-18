c17448934c48138cf525aa4743c93fc8302e2c6d
Gautham <bgautham27@gmail.com>
2023-09-18 23:14:43 +0530

    Add Better UI Balance Section
    Signed-off-by: Gautham <bgautham27@gmail.com>

--- lib/Screens/Home_Screen.dart
+++ lib/Screens/Home_Screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../Data/Expense_data.dart';
 import 'Settings.dart';
 import 'package:provider/provider.dart';
+import 'package:spendz/utils.dart';
 
 
 
@@ -21,6 +22,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
   @override
   Widget build(BuildContext context) {
+    double baseWidth = 375;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
     return Consumer<ExpenseData>(
       builder: (context,value,child) => Scaffold(
         body: CustomScrollView(
@@ -55,23 +59,133 @@ class _HomeScreenState extends State<HomeScreen> {
                 )
               ],
             ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                  return Container();
-                },
-                childCount: 10,
-              ),
-            ),
+            // SliverList(
+            //   delegate: SliverChildBuilderDelegate(
+            //         (BuildContext context, int index) {
+            //       return Container();
+            //     },
+            //     childCount: 10,
+            //   ),
+            // ),
             SliverToBoxAdapter(
               child: Column(
                 children: [
                   Container(
-                    padding:const EdgeInsets.all(16.0),
-                    alignment: Alignment.center,
-                    child: Text(
-                      'Balance Left: \$${balance.toStringAsFixed(2)}',
-                      style:const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    // group73E9 (274:230)
+                    margin: EdgeInsets.fromLTRB(16*fem, 10*fem, 16*fem, 16*fem),
+                    // padding: EdgeInsets.fromLTRB(16*fem, 24*fem, 16*fem, 23*fem),
+                    width: double.infinity,
+                    height: 110*fem,
+                    decoration: BoxDecoration (
+                      color: Color(0xfff2f2f7),
+                      borderRadius: BorderRadius.circular(13*fem),
+                    ),
+                    child: Stack(
+                      children: [
+                        // Positioned(
+                        //   // rectangle3n3j (203:17)
+                        //   left: 120*fem,
+                        //   top: 42*fem,
+                        //   child: ImageFiltered(
+                        //     imageFilter: ImageFilter.blur (
+                        //       sigmaX: 40*fem,
+                        //       sigmaY: 40*fem,
+                        //     ),
+                        //     child: Align(
+                        //       child: SizedBox(
+                        //         width: 343*fem,
+                        //         height: 110*fem,
+                        //         child: Container(
+                        //           decoration: BoxDecoration (
+                        //             borderRadius: BorderRadius.circular(24*fem),
+                        //             color: const Color(0x4c007aff),
+                        //           ),
+                        //         ),
+                        //       ),
+                        //     ),
+                        //   ),
+                        // ),
+                        Positioned(
+                          // rectangle2erd (198:15)
+                          left: 0*fem,
+                          top: 0*fem,
+                          child: Align(
+                            child: SizedBox(
+                              width: 343*fem,
+                              height: 110*fem,
+                              child: Container(
+                                decoration: BoxDecoration (
+                                  borderRadius: BorderRadius.circular(24*fem),
+                                  color:const Color(0x99007aff),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          // Aa5 (198:16)
+                          left: 16*fem,
+                          top: 45*fem,
+                          child: Align(
+                            child: SizedBox(
+                              width: 140*fem,
+                              height: 42*fem,
+                              child: Text(
+                                '₹$balance',
+                                style: SafeGoogleFont (
+                                  'Inter',
+                                  fontSize: 34*ffem,
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.2125*ffem/fem,
+                                  letterSpacing: -0.3700000048*fem,
+                                  color: const Color(0xffffffff),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          // mybalanceTZB (198:17)
+                          left: 16*fem,
+                          top: 24*fem,
+                          child: Align(
+                            child: SizedBox(
+                              width: 83*fem,
+                              height: 20*fem,
+                              child: Text(
+                                'My Balance',
+                                style: SafeGoogleFont (
+                                  'Inter',
+                                  fontSize: 15*ffem,
+                                  fontWeight: FontWeight.w400,
+                                  height: 1.3333333333*ffem/fem,
+                                  color: Color(0xffffffff),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          // xkq (198:18)
+                          left: 305*fem,
+                          top: 38*fem,
+                          child: Align(
+                            child: SizedBox(
+                              width: 11*fem,
+                              height: 18*fem,
+                              child: Center(
+                                child: Center(
+                                  child: Icon(
+                                    Icons.navigate_next_rounded, // Replace 'Icons.star' with the Material Icon you want
+                                    size: 35 * fem,
+                                    color: Colors.white, // You can specify the color you want
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   Container(
