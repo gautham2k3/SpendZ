96c88cee23ff51472ac66735d2e2bddf91bbc545
Gautham <bgautham27@gmail.com>
2023-09-28 23:23:40 +0530

    Added Frosted Glass Design Widget
    Signed-off-by: Gautham <bgautham27@gmail.com>

---
+++ lib/Design/FrostedGlass.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+class FrostedGlassBox extends StatelessWidget {
+  const FrostedGlassBox(
+      {Key? key,
+        required this.theWidth,
+        required this.theHeight,
+        required this.theChild})
+      : super(key: key);
+
+  final theWidth;
+  final theHeight;
+  final theChild;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(30),
+      child: Container(
+        width: theWidth,
+        height: theHeight,
+        color: Colors.transparent,
+        //we use Stack(); because we want the effects be on top of each other,
+        //  just like layer in photoshop.
+        child: Stack(
+          children: [
+            //blur effect ==> the third layer of stack
+            BackdropFilter(
+              filter: ImageFilter.blur(
+                //sigmaX is the Horizontal blur
+                sigmaX: 4.0,
+                //sigmaY is the Vertical blur
+                sigmaY: 4.0,
+              ),
+              //we use this container to scale up the blur effect to fit its
+              //  parent, without this container the blur effect doesn't appear.
+              child: Container(),
+            ),
+            //gradient effect ==> the second layer of stack
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: Colors.white.withOpacity(0.13)),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      //begin color
+                      Colors.white.withOpacity(0.15),
+                      //end color
+                      Colors.white.withOpacity(0.05),
+                    ]),
+              ),
+            ),
+            //child ==> the first/top layer of stack
+            Center(child: theChild),
+          ],
+        ),
+      ),
+    );
+  }
+}
