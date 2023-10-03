558a3619b41823d3615324b974b1cc434f7335b0
Gautham <bgautham27@gmail.com>
2023-10-03 22:36:00 +0530

    Added Balance Section
    Signed-off-by: Gautham <bgautham27@gmail.com>

--- lib/Screens/Balance_Overview.dart
+++ lib/Screens/Balance_Overview.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../Design/FrostedGlass.dart';
 
 class BalanceOverview extends StatefulWidget {
   final int number;
@@ -9,34 +10,74 @@ class BalanceOverview extends StatefulWidget {
 
 class _BalanceOverviewState extends State<BalanceOverview> {
   String fact = 'Loading';
+  String? balanceO;
+  @override
+  void initState() {
+    super.initState();
+    balanceO = widget.number.toString();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-            icon:const Icon(Icons.navigate_before_rounded),
-            iconSize: 30,
-            onPressed: () {
-              Navigator.pop(context);
-            }
+          icon: const Icon(Icons.navigate_before_rounded),
+          iconSize: 30,
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-        title:const Text('Balance Overview'),
+        title: const Text('Balance Overview'),
       ),
       body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/Balance_Overview_Background.jpg'),
+            fit: BoxFit.cover,
+          ),
         ),
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: FrostedGlassBox(
+                theWidth: 330.0,
+                theHeight: 50.0,
+                theChild: Text(
+                  'Balance Left : $balanceO',
+                  style: const TextStyle(color: Colors.white70, fontSize: 20.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: FrostedGlassBox(
+                theWidth: 330.0,
+                theHeight: 100.0,
+                theChild: Text(
+                  fact,
+                  style:const TextStyle(
+                    color: Colors.white54,
+                  ),
+                ),
+              ),
+            ),
+            // Add more FrostedGlassBox widgets as needed
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         focusColor: Colors.white54,
-        onPressed: () {
-
-        },
+        onPressed: () {},
         shape: RoundedRectangleBorder(
-            side: const BorderSide(width: 3,color: Colors.white54),
-            borderRadius: BorderRadius.circular(100)
+          side: const BorderSide(width: 3, color: Colors.white54),
+          borderRadius: BorderRadius.circular(100),
         ),
         child: const Icon(Icons.refresh),
       ),
-
     );
   }
 }
