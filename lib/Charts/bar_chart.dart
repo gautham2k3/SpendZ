9d6fc794131e17374807e7b1b498d8ab2d409d07
Gautham <bgautham27@gmail.com>
2023-10-28 23:53:44 +0530

    Updated Bar Chart Data

--- lib/Charts/bar_chart.dart
+++ lib/Charts/bar_chart.dart
@@ -1,14 +1,41 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-
+import 'package:fl_chart/fl_chart.dart';
 import 'bar_data.dart';
 
 class myBarGraph extends StatelessWidget {
-  myBarGraph({Key key}) : super(key: key);
+
+  final double? maxY;
+  final double sunAmount;
+  final double monAmount;
+  final double tueAmount;
+  final double wedAmount;
+  final double thuAmount;
+  final double friAmount;
+  final double satAmount;
+
+  const myBarGraph(this.maxY, this.sunAmount, this.monAmount, this.tueAmount,
+      this.wedAmount, this.thuAmount, this.friAmount, this.satAmount, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return null;
+    BarData myBarGraphData = BarData(sunAmount, monAmount, tueAmount, wedAmount, thuAmount, friAmount, satAmount);
+    myBarGraphData.initilizeBar();
+    return BarChart(BarChartData(
+      maxY: maxY,
+      minY: 0,
+      barGroups: myBarGraphData.barData.map(
+            (data) => BarChartGroupData(x: data.x,barRods: [
+          BarChartRodData(
+            toY: data.y,
+            width: 12,
+            color: Colors.lightBlueAccent,
+            borderSide: BorderSide(color: Colors.black38),
+            borderRadius: BorderRadius.circular(3),
+          )
+        ],
+        ),
+      ).toList(),
+    ));
   }
 }
