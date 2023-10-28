65f549a00af312422e40a5952f791feb5fbe4d01
Gautham <bgautham27@gmail.com>
2023-10-28 23:49:33 +0530

    Added Bar Chart

---
+++ lib/Charts/bar_chart.dart
@@ -0,0 +1,14 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+import 'bar_data.dart';
+
+class myBarGraph extends StatelessWidget {
+  myBarGraph({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
